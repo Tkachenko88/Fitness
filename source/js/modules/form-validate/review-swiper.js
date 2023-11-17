81bb21f9f0fe23199a2fb8656aa0749504224039
Tkachenko88 <vanes_09@inbox.ru>
2023-11-17 09:05:26 +0300

    footer

--- source/js/modules/form-validate/review-swiper.js
+++ source/js/modules/form-validate/review-swiper.js
@@ -9,5 +9,22 @@ export const initReviewSlider = () => {
       nextEl: buttonNext,
       prevEl: buttonPrev,
     },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      // when window width is >= 640px
+      1366: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+    },
   });
 };
